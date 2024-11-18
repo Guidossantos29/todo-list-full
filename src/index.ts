a8d827c981fee0bd20ed960e78b5827d94a76cd3
Luiz Guilherme <lguidossantos29@gmail.com>
2024-11-18 17:41:26 -0300

    feat:created tarefa create function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request,Response }  from "express"
+import { Tarefa } from './../node_modules/.prisma/client/index.d';
+import express, { Request, Response } from "express"
 import cors from 'cors';
 import { PrismaClient } from "@prisma/client";
 
@@ -9,8 +10,36 @@ const prisma = new PrismaClient();
 app.use(cors())
 app.use(express.json())
 
-app.get("/tarefas", async (req: Request,res:Response) => {
+app.get("/tarefas", async (req: Request, res: Response) => {
     const tarefas = await prisma.tarefa.findMany({
-        orderBy: { ordemApresentacao:"asc" }
+        orderBy: { ordemApresentacao: "asc" }
     })
+
+    res.json(tarefas)
 })
+app.post("tarefas", async (req: Request, res: Response) => {
+    const { nome, custo, datalimite } = req.body
+    const ordemApresentacao = (await prisma.tarefa.count()) + 1;
+    
+
+    try {
+        const Tarefa = await prisma.tarefa.create({
+            data: {
+                nome,
+                custo,
+                dataLimite: new Date(datalimite),
+                ordemApresentacao,
+    
+            },
+    
+        })
+        res.json(Tarefa)
+
+    } catch(error){
+        if(error instanceof Error){
+            res.status(400).json({ error: error.message });
+        }
+       
+    }
+})
+
